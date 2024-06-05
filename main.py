a02a04ead3c759f2d471d128a8854239d9050a6f
Jon 赚大钱 <janders@radix.trade>
2024-06-05 14:48:49 -0500

    update requirements Workzeug

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+import os
 
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def say_hello():
     return "Hello, world!"
 
-
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
